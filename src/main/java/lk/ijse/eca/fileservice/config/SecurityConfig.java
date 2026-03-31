74c62e5a06d50ed1bbfb3967db482988a819e20a
Vinan <vinanjayasinghe3@gmail.com>
2026-03-31 07:28:09 +0530

    fix changes

---
+++ src/main/java/lk/ijse/eca/fileservice/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package lk.ijse.eca.fileservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/api/v1/files/**").permitAll() // Meeka dammahama token illanne na jwt token eka disable karanna
+                .anyRequest().authenticated()
+            );
+        return http.build();
+    }
+
+}
